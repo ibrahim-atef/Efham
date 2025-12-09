d717f32479372327e279ca845e173018ed05963d
Ibrahim Elbaz <ibrahim.elbaz.1011@gmail.com>
2025-12-09 17:01:23 +0200

    update app logo

--- lib/common/utils/app_text.dart
+++ lib/common/utils/app_text.dart
@@ -1,9 +1,19 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations_ar.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations_en.dart';
 import 'package:webinar/common/data/app_language.dart';
 import 'package:webinar/locator.dart';
 
 AppLocalizations get appText {
   final currentLanguage = locator<AppLanguage>().currentLanguage.toLowerCase();
-  return lookupAppLocalizations(Locale(currentLanguage));
+
+  // Return the appropriate localization instance based on current language
+  switch (currentLanguage) {
+    case 'ar':
+      return AppLocalizationsAr();
+    case 'en':
+      return AppLocalizationsEn();
+    default:
+      return AppLocalizationsAr(); // Default to Arabic
+  }
 }
